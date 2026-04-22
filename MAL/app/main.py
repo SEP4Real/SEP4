cd5ec2ace6877a58819a8dcb92f9f6a573d2fa9f
Eduard Fekete <edko.fek@gmail.com>
2026-04-22 09:34:42 +0200

    [Update] Handled secrets with .env

--- MAL/app/main.py
+++ MAL/app/main.py
@@ -14,12 +14,19 @@ def hello_world() -> dict[str, str]:
 
 @app.get("/db-check")
 def db_check() -> dict[str, str | int]:
-    database_url = os.getenv(
-        "DATABASE_URL",
-        "postgresql://study:study@postgres:5432/sep4",
-    )
+    db_host = os.environ["DB_HOST"]
+    db_port = os.environ["DB_PORT"]
+    db_name = os.environ["DB_NAME"]
+    db_user = os.environ["DB_USER"]
+    db_password = os.environ["DB_PASSWORD"]
 
-    with psycopg.connect(database_url) as conn:
+    with psycopg.connect(
+        host=db_host,
+        port=db_port,
+        dbname=db_name,
+        user=db_user,
+        password=db_password,
+    ) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT 1")
             result = cur.fetchone()
