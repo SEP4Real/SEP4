9d423dc24c3b63bff5b989080c8a3bcfb4fb28e1
Karina Rubahova <karina.rubahova@gmail.com>
2026-05-23 13:42:02 +0200

    Language context tests

--- Frontend/src/pages/Dashboard.test.jsx
+++ Frontend/src/pages/Dashboard.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-env vitest */
 import { render, screen } from "@testing-library/react";
 import EmptyState from "../components/EmptyState";
 import LoadingSpinner from "../components/LoadingSpinner";
